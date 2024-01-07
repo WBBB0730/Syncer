627f8beef3bbe70bd83c6b8d18347f9ffff82128
WBBB0730 <1339523637@qq.com>
2024-01-08 00:17:07 +0800

    fix: 移动端通知优化

--- syncer-mobile/src/utils/notify.js
+++ syncer-mobile/src/utils/notify.js
@@ -1,10 +1,12 @@
 import PushNotification from 'react-native-push-notification'
+import { AppState } from 'react-native'
 
 export function notify(title, content) {
+  if (AppState.currentState === 'active')
+    return
   PushNotification.localNotification({
     channelId: 'syncer',
     title,
     message: content,
-    ongoing: true
   })
 }
